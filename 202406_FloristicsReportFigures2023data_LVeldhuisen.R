1f9b749698fa53627b56229e63f53669e3ae1e7f
leah.veldhuisen <leah.veldhuisen@gmail.com>
2024-06-04 13:19:42 -0600

    update

---
+++ 202406_FloristicsReportFigures2023data_LVeldhuisen.R
@@ -0,0 +1,11 @@
+#2024-06-04
+#code for figures for CommonGround experimental plots 2023 data
+
+library(tidyverse) #for making figures
+library(dplyr) #data manipulation
+library(tidyr) #more data manipulation
+library(viridis) #colorblind friendly color palette
+
+#2023 data for experimental plots-----------------------------------------------
+#bring in and clean up data-----------------------------------------------------
+
